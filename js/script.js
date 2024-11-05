f6c4b7b892c30520657711cab888fe0823e711d9
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-11-05 10:17:28 -0800

    Contact Form added.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ function userScroll() {
   
   document.addEventListener('DOMContentLoaded', userScroll);
 // |-----| Scroll Effect function |-----| //
+
+// |-----| SweetAlert2 "Contact" alert |-----| //
+function sendMessage() {
+  Swal.fire({
+      title: "Message sent!",
+      text: "Your message will be delivered to Abril",
+      icon: "success",
+
+      confirmButtonColor: '#6C2AD9',
+      confirmButtonText: 'Entendido'
+  });
+}
+// |-----| SweetAlert2 "Contact" alert |-----| //

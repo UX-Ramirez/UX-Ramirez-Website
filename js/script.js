1e54d041e4785d92ecbc7764fd3c600c3b825f57
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-11-25 21:41:07 -0800

    "To Top" btn added.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 // |-----| Scroll Effect function |-----| //
 function userScroll() {
     const navbar = document.querySelector('.navbar');
-  
+    const toTopButton = document.getElementById("to-top");
+
     window.addEventListener('scroll', () => {
       if (window.scrollY > 35) {
+        toTopButton.classList.add("show");
         navbar.classList.add('bg-dark');
       } else {
+        toTopButton.classList.remove("show");
         navbar.classList.remove('bg-dark');
       }
     });
@@ -13,6 +16,13 @@ function userScroll() {
   
   document.addEventListener('DOMContentLoaded', userScroll);
 // |-----| Scroll Effect function |-----| //
+
+// |-----| Scroll To Top function |-----| //
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// |-----| Scroll To Top function |-----| //
 
 // |-----| SweetAlert2 "Contact" alert |-----| //
 document.getElementById('contactForm').addEventListener('submit', function(event) {
